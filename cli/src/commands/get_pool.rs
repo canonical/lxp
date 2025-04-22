222ee747b29dbff95510faec923a2d3337c2aaa9
Thinking-Dragon <clement.gassmann-prince@canonical.com>
2025-04-22 13:33:15 -0400

    Add title to "lxp pool <name>" result

--- cli/src/commands/get_pool.rs
+++ cli/src/commands/get_pool.rs
@@ -13,14 +13,11 @@ pub struct GetPoolArgs {
 pub fn run_command_get_pool(lxp: &mut LinuxPoolConnector, args: GetPoolArgs) -> Result<()> {
     let pool: PoolDefinition = lxp.get_pool(args.pool_name)?;
 
-    let title: String = "      Pool".bold();
-    println!("{}: {}", title, pool.name);
-    let title: String = "   Base OS".bold();
-    println!("{}: {}", title, pool.base);
-    let title: String = "Live count".bold();
-    println!("{}: {} (number of machines running at any point in time)", title, pool.live_count);
-    let title: String = " Pool size".bold();
-    println!("{}: {} (number of machines in pool)", title, pool.pool_size);
+    println!("\n{}\n", "Pool information".underlined());
+    println!("{}: {}", "      Name".bold(), pool.name);
+    println!("{}: {}", "   Base OS".bold(), pool.base);
+    println!("{}: {} (number of machines running at any point in time)", "Live count".bold(), pool.live_count);
+    println!("{}: {} (number of machines in pool)\n", " Pool size".bold(), pool.pool_size);
 
     Ok(())
 }
